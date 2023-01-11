3264c0607cf6a74b55b9bcbc90f09c6900dc5ba2
Mateo <mateo-sanchez@hotmail.com>
2023-01-11 17:50:37 -0300

    todos los botones

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,106 +10,122 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ScafoldApp',
-      //disable debug banner
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      title: 'Botones',
+      home: MyHomePage(
+        title: 'Botones',
       ),
-      home: const MyHomePage(title: 'ScafoldApp Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  const MyHomePage({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  final String title;
+  const MyHomePage({this.title, super.key});
+  final String? title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Icon _corazon = const Icon(Icons.favorite_border, color: Colors.white);
-  bool _corazonPulsado = false;
-
-  void _like() {
-    setState(() {
-      if (_corazonPulsado) {
-        _corazon = const Icon(Icons.favorite_border, color: Colors.white);
-        _corazonPulsado = false;
-      } else {
-        _corazon = const Icon(Icons.favorite, color: Colors.red);
-        _corazonPulsado = true;
-      }
-    });
-  }
+  String botonPulsado = 'ninguno';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            onPressed: _like,
-            icon: _corazon,
-          ),
-        ],
+        title: Text(widget.title!),
       ),
-      body: const Center(
-        child: Text(
-          'Hola Pauli',
-          style: TextStyle(fontSize: 30, color: Colors.blueGrey),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('El botón pulsado es:',
+                style: const TextStyle(fontSize: 20)),
+            Text(
+              botonPulsado,
+              style: const TextStyle(fontSize: 40, color: Colors.blue),
+            ),
+            const Padding(padding: EdgeInsets.all(20)), // Separador
+            ElevatedButton(
+              onPressed: () => _wichButtonPressed('ElevatedButton'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                onPrimary: Colors.white,
+                elevation: 10,
+                shadowColor: Colors.red,
+              ),
+              child: const Text('ElevatedButton'),
+            ),
+            const Padding(padding: EdgeInsets.all(5)), // Separador
+            TextButton(
+              onPressed: () => _wichButtonPressed('TextButton'),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text('TextButton'),
+            ),
+            const Padding(padding: EdgeInsets.all(5)), // Separador
+            IconButton(
+              onPressed: () => _wichButtonPressed('IconButton'),
+              icon: const Icon(Icons.accessibility_new_rounded),
+              color: Colors.black,
+              iconSize: 50,
+            ),
+            const Padding(padding: EdgeInsets.all(5)), // Separador
+            Ink(
+              decoration: ShapeDecoration(
+                shape: const CircleBorder(),
+                color: Colors.blue,
+              ),
+              child: IconButton(
+                onPressed: () => _wichButtonPressed('IconButton with Ink'),
+                icon: const Icon(Icons.accessibility),
+                color: Colors.white,
+                iconSize: 50,
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(5)), // Separador
+            OutlinedButton(
+              onPressed: () => _wichButtonPressed('OutlinedButton'),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.blue,
+                side: const BorderSide(color: Colors.blue, width: 2),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text('OutlinedButton'),
+            ),
+            const Padding(padding: EdgeInsets.all(5)), // Separador
+            CupertinoButton(
+              onPressed: () => _wichButtonPressed('CupertinoButton'),
+              color: Colors.blue,
+              child: const Text('CupertinoButton'),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _like,
-        tooltip: 'Like',
-        backgroundColor: Colors.blueGrey,
-        //Si no es extended, el icono va como child y no se puede poner texto
-        child: _corazon,
-        //label: const Text('Like'),
-      ),
-      //Se puede poner el botón en donde queramos
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.blueGrey,
-        shape: CircularNotchedRectangle(),
-        child: Container(
-          height: 50.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.home, color: Colors.white),
-              ),
-              // IconButton(
-              //   onPressed: () {},
-              //   icon: const Icon(Icons.search, color: Colors.white),
-              // ),
-              // IconButton(
-              //   onPressed: () {},
-              //   icon: const Icon(Icons.add, color: Colors.white),
-              // ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.person, color: Colors.white),
-              ),
-            ],
-          ),
-        ),
+        onPressed: _floatingActionButtonPressed,
+        tooltip: 'Press me',
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
+  }
+
+  void _floatingActionButtonPressed() {
+    _wichButtonPressed('FloatingActionButton');
+  }
+
+  void _wichButtonPressed(String buttonName) {
+    setState(() {
+      botonPulsado = buttonName;
+    });
   }
 }
