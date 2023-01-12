09bbf682fc2f508a63c7dd5059b44dbf6710f2ec
Mateo <mateo-sanchez@hotmail.com>
2023-01-12 17:12:03 -0300

    arrancando

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,58 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(
+    title: 'Imagenes',
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Botones',
-      home: MyHomePage(
-        title: 'Botones',
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Imagenes'),
+        ),
+        body: MyHomePage(title),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({this.title, super.key});
-  final String? title;
+  MyHomePage(this.title, {Key? key}) : super(key: key);
+
+  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String botonPulsado = 'ninguno';
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('El botón pulsado es:',
-                style: const TextStyle(fontSize: 20)),
-            Text(
-              botonPulsado,
-              style: const TextStyle(fontSize: 40, color: Colors.blue),
-            ),
-            const Padding(padding: EdgeInsets.all(20)), // Separador
-            ElevatedButton(
-              onPressed: () => _wichButtonPressed('ElevatedButton'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.blue,
-                onPrimary: Colors.white,
-                elevation: 10,
-                shadowColor: Colors.red,
-              ),
-              child: const Text('ElevatedButton'),
-            ),
-            const Padding(padding: EdgeInsets.all(5)), // Separador
-            TextButton(
-              onPressed: () => _wichButtonPressed('TextButton'),
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: const Text('TextButton'),
-            ),
-            const Padding(padding: EdgeInsets.all(5)), // Separador
-            IconButton(
-              onPressed: () => _wichButtonPressed('IconButton'),
-              icon: const Icon(Icons.accessibility_new_rounded),
-              color: Colors.black,
-              iconSize: 50,
-            ),
-            const Padding(padding: EdgeInsets.all(5)), // Separador
-            Ink(
-              decoration: ShapeDecoration(
-                shape: const CircleBorder(),
-                color: Colors.blue,
-              ),
-              child: IconButton(
-                onPressed: () => _wichButtonPressed('IconButton with Ink'),
-                icon: const Icon(Icons.accessibility),
-                color: Colors.white,
-                iconSize: 50,
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)), // Separador
-            OutlinedButton(
-              onPressed: () => _wichButtonPressed('OutlinedButton'),
-              style: OutlinedButton.styleFrom(
-                primary: Colors.blue,
-                side: const BorderSide(color: Colors.blue, width: 2),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: const Text('OutlinedButton'),
-            ),
-            const Padding(padding: EdgeInsets.all(5)), // Separador
-            CupertinoButton(
-              onPressed: () => _wichButtonPressed('CupertinoButton'),
-              color: Colors.blue,
-              child: const Text('CupertinoButton'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _floatingActionButtonPressed,
-        tooltip: 'Press me',
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add, color: Colors.white),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+            width: 200,
+            height: 200,
+          ),
+          Image.asset(
+            '/assets/images/imagen1.png',
+            width: 200,
+            height: 200,
+          ),
+        ],
       ),
     );
-  }
-
-  void _floatingActionButtonPressed() {
-    _wichButtonPressed('FloatingActionButton');
-  }
-
-  void _wichButtonPressed(String buttonName) {
-    setState(() {
-      botonPulsado = buttonName;
-    });
   }
 }
