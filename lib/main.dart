c8ca9c37dfff75f5b775ce42116debb55adf0e06
Mateo <mateo-sanchez@hotmail.com>
2023-01-14 00:54:52 -0300

    snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_testing/control_contador.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,63 +8,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'StatefullAPP',
+      title: 'SnackApp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'StatefullAPP Home Page'),
+      home: const MyHomePage(title: 'Snackbar App Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
-  final String title;
+  final String? title;
 
   @override
-  State createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final ControlContador _controlContador = ControlContador();
-
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: _controlContador.stream,
-        initialData: 0,
-        builder: (context, snapshot) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(widget.title),
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ],
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: _controlContador.incrementar,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ),
-          );
-        });
-  }
-
-  @override
-  void dispose() {
-    _controlContador.dispose();
-    super.dispose();
+    final _snackBar = SnackBar(
+      content: const Text('Yay! A SnackBar!'),
+      duration: const Duration(seconds: 5),
+      action: SnackBarAction(
+        label: 'Cerrar',
+        onPressed: () {
+          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+        },
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Show SnackBar'),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+          },
+        ),
+      ),
+    );
   }
 }
